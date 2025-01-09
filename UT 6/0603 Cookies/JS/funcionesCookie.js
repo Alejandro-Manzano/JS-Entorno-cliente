52ef22073898757f263dcb33c9c639fc8b6f977d
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2025-01-09 10:48:38 +0100

    cookies js

--- UT 6/0603 Cookies/JS/funcionesCookie.js
+++ UT 6/0603 Cookies/JS/funcionesCookie.js
@@ -1,12 +1,23 @@
-function manageCookie(name,value,days="") {
-    
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${name}=${value};${expires};path=/`;
 }
 
-function readCookie(name) {
-    
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const [key, val] = cookies[i].trim().split('=');
+        if (key === name) return val;
+    }
+    return null;
 }
 
+function deleteCookie(name) {
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/`;
+}
 
-function delAllCookies() {
-    
+function listCookies() {
+    return document.cookie.split(';').map(cookie => cookie.trim());
 }
